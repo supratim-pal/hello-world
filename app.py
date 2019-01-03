915e1462bc429452e78dada6c30550916e54f32f
supratim-pal <45565566+supratim-pal@users.noreply.github.com>
2019-01-04 00:10:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -96,10 +96,9 @@ async def ws_handler(websocket, path):
         print("")
 
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5687))
-    start_server = websockets.serve(ws_handler, '', port, klass=HttpWSSProtocol)
-    # logger.info('Listening on port %d', port)
+port = int(os.getenv('PORT', 5687))
+start_server = websockets.serve(ws_handler, '', port, klass=HttpWSSProtocol)
+# logger.info('Listening on port %d', port)
 
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
