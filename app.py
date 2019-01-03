0e1ff93f9a4067254547ad9880997f246bed1d19
supratim-pal <45565566+supratim-pal@users.noreply.github.com>
2019-01-04 01:29:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ async def ws_handler(websocket, path):
         data ='{"empty":"empty"}'
         while True:
             data = await websocket.recv()
-            print(data)
+            #print(data)
             updateData(data)
     except Exception as e:
         print(e)
